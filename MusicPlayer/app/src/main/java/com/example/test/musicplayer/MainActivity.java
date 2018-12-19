05c5962d49caecf365c9e4a0e5e992665ab1d7c3
InhyeJeong <inhyejeong59@gmail.com>
2018-12-20 01:21:33 +0900

    MusicPlayer 수정

--- MusicPlayer/app/src/main/java/com/example/test/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/test/musicplayer/MainActivity.java
@@ -15,36 +15,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    boolean bReadPerm = false;
-    Button button_play, button_stop;
-    boolean bStatePlay = false;
+    boolean bReadPerm = false;  //SD카드를 읽기 위한 여부를 저장
+    Button button_play, button_stop; // 버튼 객체를 위한 변수
+    boolean bStatePlay = false;  // 재생 상태 유무를 위한 변수,
+    // true : 재생, false : 정지
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // sd 카드 읽기를 위한 권한을 처리하는 함수
         setPermission();
-
+        // 버튼 객체를 생성
         button_play = (Button) findViewById(R.id.button_play);
         button_stop = (Button) findViewById(R.id.button_stop);
-
+        // 버튼의 리스너 등록
         button_play.setOnClickListener(new myButtonListener());
         button_stop.setOnClickListener(new myButtonListener());
-
-        registerReceiver(receiver, new IntentFilter("rj.myplayerservice"));
+        // 서비스와 통신을 위해 리시버를 등록
+        registerReceiver(receiver, new IntentFilter("com.example.test.musicplayer"));
 
         if(bReadPerm) {
+            // SD 카드의 상태를 확인
             String state = Environment.getExternalStorageState();
 
             if (state.equals(Environment.MEDIA_MOUNTED)) {
+                // SD카드가 장착이 되어 있다면
                 try {
+                    // SD 카드 안에 있는 mp3 파일의 경로를 읽어온다.
                     String musicPath = Environment.getExternalStorageDirectory().getAbsolutePath()
                             + "/paulkim.mp3";
+                    // 인텐트에 mp3 파일 경로를 저장한다.
                     Intent intent = new Intent(MainActivity.this, PlayService.class);
                     intent.putExtra("filePath", musicPath);
+                    // 서비스를 시작한다.
                     startService(intent);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -59,20 +66,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d("MyPlayerService_log", "Main onDestroy()");
+        // 엑티비티가 사라질 때 리시버도 해제
         unregisterReceiver(receiver);
     }
 
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            // 서비스로 부터 전달된 인텐트를 state라는 키 값으로
+            // 읽어온다.
+            // 값이 없다면, state 변수 안에는 null, play, pause, stop
             String state = intent.getStringExtra("state");
 
-            if(state != null) {
+            if(state != null) {// state라는 키에 맞는 값이 있다.
+                // 서비스가 재생 명령을 수행한 결과가 있다면
                 if (state.equals("play")) {
+                    // 재생중임을 표시하는 bStatePlay 변수에 true 저장
                     bStatePlay = true;
+                    // 버튼에 표시하는 문자를 pause로 바꾼다.
                     button_play.setText("Pause");
                 } else if(state.equals("pause") || state.equals("stop")) {
+                    // 서비스가 중지 혹은 일시 중지 명령을 수행한 결과가
+                    // 있다면
+
+                    // 재생중임을 표시하는 bStatePlay 변수에 false 저장
                     bStatePlay = false;
+                    // 버튼에 표시하는 문자를 play로 바꾼다.
                     button_play.setText("Play");
                 }
             }
@@ -84,19 +103,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
-            intent = new Intent("rj.myplayerservice");
+            // 서비스의 리시버에 전달하기 위한 인텐트 준비
+            intent = new Intent("com.example.test.musicplayer");
             switch(view.getId()) {
+                // play & pause 버튼
                 case R.id.button_play:
-                    if(bStatePlay) {
+                    Toast.makeText(MainActivity.this, "실행", Toast.LENGTH_SHORT).show();
+                    if(bStatePlay) {// 음악이 재생중이면
+                        // play & pause 버튼이 일시 중지 명령을 인텐트에 포함
                         intent.putExtra("btn", "pause");
                     } else {
+                        // play & pause 버튼이 재생 명령을 인텐트에 포함
                         intent.putExtra("btn", "play");
                     }
                     break;
+                // stop 버튼
                 case R.id.button_stop:
+                    // stop 명령 인텐트에 포함
                     intent.putExtra("btn", "stop");
                     break;
             }
+            // 엑티비티에서 서비스의 브로드케스트 리시버에게 인텐트를 전달
             sendBroadcast(intent);
         }
     }
--- MusicPlayer/app/src/main/java/com/example/test/musicplayer/PlayService.java
+++ MusicPlayer/app/src/main/java/com/example/test/musicplayer/PlayService.java
@@ -15,33 +15,48 @@ import android.util.Log;
 
 public class PlayService extends Service {
 
-    MediaPlayer player;
-    String filePath;
+    MediaPlayer player; // mp3파일을 재생하는 MediaPlayer 객체 변수
+    String filePath;// mp3파일의 경로를 저장하는 변수
 
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            // 액티비티에서 브로드캐스팅 된 인텐트를 리시버가 받아서
+            // 인텐트 안에 저장된 내용을 읽어본다.
             String btn = intent.getStringExtra("btn");
 
-            Intent intent1 = new Intent("rj.myplayerservice");
+            // 서비스에서 액티비티로 응답하기 위한 인텐트트
+            Intent intent1 = new Intent("com.example.test.musicplayer");
+            // btn 안의 값이 null 값이 없음, play, stop, pause 중 1
             if(btn != null) {
                 if(btn.equals("play") || btn.equals("pause")) {
+                    // 현재 파일이 재생중이면
                     if(player.isPlaying()) {
+                        // 일시 중지 기능을 수행
                         player.pause();
+
+                        // 일시 중지 기능을 수행한 상태를 인텐트에 기록
                         intent1.putExtra("state", "pause");
                     } else {
+                        // 재생 기능 수행
                         player.start();
+
+                        // 재생 기능을 수행한 상태를 인텐트에 기록
                         intent1.putExtra("state", "play");
                     }
                 } else if(btn.equals("stop")) {
+                    // 재생을 정지 하는 기능 수행
                     player.stop();
                     try {
+                        // 중지 기능을 수행한 상태를 인텐트에 기록
                         intent1.putExtra("state", "stop");
                         player.prepare();
                     } catch(Exception e) {
                         e.printStackTrace();
                     }
                 }
+                // 수행한 상태를 기록한 인텐드를 패키지 내에서
+                // 브로드 캐스팅 한다.
                 sendBroadcast(intent1);
             }
         }
@@ -49,18 +64,32 @@ public class PlayService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        // 처음 실행할 때 엑티비티로 부터 받은 인텐트에서
+        // 음악파일의 경로를 얻어온다.
+
+        // null 혹은 SD카드 안의 mp3 파일의 경로가 filePath 변수에 저장
         filePath = intent.getStringExtra("filePath");
         if(filePath != null) {
+            // 파일의 경로가 있다면
             try {
+                // mediaplayer에서 음악 재생이 완료되면
+                // 호출되는 OnCompletionListener()를 활용해서
+                // 음악 파일의 재생이 완료 되었다면
+                // 엑티비디에 결과를 보고해야 한다.
                 player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     @Override
                     public void onCompletion(MediaPlayer mediaPlayer) {
-                        Intent intent = new Intent("rj.myplayerservice");
+                        // 액티비티의 리시버에게 전달할 인텐트를 만든다.
+                        Intent intent = new Intent("com.example.test.musicplayer");
+                        // 실행한 결과를 인텐트에 기록한다.
                         intent.putExtra("state", "stop");
+                        // 인텐트를 패키지 안에서 브로드 캐스트를 한다.
                         sendBroadcast(intent);
+                        // 서비스를 종료하는 코드
                         stopSelf();
                     }
                 });
+                // 경로에 있는 파일을 읽어와서 재생을 준비
                 player.setDataSource(filePath);
                 player.prepare();
             } catch(Exception e) {
@@ -75,15 +104,17 @@ public class PlayService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        // mp3 재생을 위한 MediaPlayer 객체를 생성한다.
         player = new MediaPlayer();
-
-        registerReceiver(receiver, new IntentFilter("rj.myplayerservice"));
+        // 액티비티와 통신을 위한 리시버를 등록한다.
+        registerReceiver(receiver, new IntentFilter("com.example.test.musicplayer"));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d("MyPlayerService_log", "Service onDestroy()");
+        // 서비스가 종료될때 리시버를 등록 해제한다.
         unregisterReceiver(receiver);
     }
 
